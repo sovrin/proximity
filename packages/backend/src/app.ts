bd3bcb585e55005d6d58beadf36b363453e560c9
Oleg Kamlowski <kamlowski.oleg@gmail.com>
2020-05-15 20:10:00 +0200

    refactor, backend

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,31 +1,18 @@
 import serverFactory, {IServer} from '@sovrin/proximity-server';
-import * as middlewares from './middlewares';
-
-import open from './routes/push/open'
-import data from './routes/push/data'
-import close from './routes/push/close'
+import routes from './routes';
 
 const PORT = process.env.PORT || 3315;
 
 /**
  *
  */
-const factory = () => {
-    const app: IServer = serverFactory({port: PORT});
+const factory = async () => {
+    const {listen, open, close, register}: IServer = serverFactory({port: PORT});
 
-    for (const middleware of Object.values(middlewares)) {
-        app.use(middleware);
-    }
-
-    app.open(open);
-    app.on('push/data', data);
-    app.close(close);
-
+    await routes({open, close, register});
     console.info('listening on :' + PORT);
 
-    app.listen();
-
-    return app;
+    listen();
 };
 
 /**
