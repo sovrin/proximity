2c08708ebd1ddfebc67f67086382e230da1e2f95
Oleg Kamlowski <kamlowski.oleg@gmail.com>
2021-03-28 16:40:09 +0200

    feat, add migration stub

---
+++ packages/migration/src/index.ts
@@ -0,0 +1,23 @@
+import {utils} from '@sovrin/proximity-common';
+const factory = (root: string, to: string) => {
+
+    const up = () => {
+
+    }
+
+    const down = () => {
+
+    }
+
+    return {
+        up,
+        down
+    }
+}
+
+/**
+ * User: Oleg Kamlowski <oleg.kamlowski@thomann.de>
+ * Date: 20.10.2020
+ * Time: 21:04
+ */
+export default factory;
